8f3a18a32ce4b99fbdcad89e2a85c7b86e7cdf05
KhanhhNe <khanhluong3005@gmail.com>
2021-05-20 15:11:28 +0700

    Full features.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+import os
+from datetime import time
+
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -6,10 +9,18 @@ app = Flask(__name__)
 @app.route('/')
 def main_route():
     if request.method == 'GET':
-        return render_template('index.html')
+        imgs = [os.path.join('imgs', filename) for filename in os.listdir('imgs') if 'labelled' not in filename]
+        time_offset = sum([14 * 60**2, 16 * 60, 4]) * 1000
+
+        return render_template('index.html', imgs=imgs[:1], time_offset=time_offset, pixel_scale=10)
     else:
         pass
 
 
+@app.route('/imgs/<path:filename>')
+def get_img(filename):
+    return send_from_directory('imgs', filename)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
