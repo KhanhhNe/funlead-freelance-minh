69aef67669d0f67d46ecefd027d1a976a0f0e80c
KhanhhNe <khanhluong3005@gmail.com>
2021-05-20 17:24:27 +0700

    Show default values before selecting range.

--- app.py
+++ app.py
@@ -9,22 +9,29 @@ app = Flask(__name__)
 def main_route():
     img = [os.path.join('imgs', filename) for filename in os.listdir('imgs') if 'labelled' not in filename][0]
     if request.args.get('time_offset'):
-        time_offset_str = request.args['time_offset']
+        time_start_str = request.args['time_offset']
     else:
-        time_offset_str = '14:16:04'
-    time_end_str = request.args.get('time_end', '')
-    hours, minutes, secs = map(int, time_offset_str.split(':'))
-    time_offset = sum([hours * 60**2, minutes * 60, secs]) * 1000
+        time_start_str = '14:16:04'
+    time_end_str = request.args.get('time_end', time_start_str)
+
+    time_start = parse_time_str(time_start_str)
+    time_end = parse_time_str(time_end_str)
 
     bit_start = int(request.args.get('bit_start', '0'))
     bit_end = int(request.args.get('bit_end', '15'))
 
     return render_template(
         'index.html', img=img, pixel_scale=10,
-        time_offset=time_offset,
         bit_start=bit_start,
-        bit_end=bit_end
+        bit_end=bit_end,
+        time_start=time_start,
+        time_end=time_end
     )
+
+
+def parse_time_str(time_str):
+    hours, minutes, secs = map(int, time_str.split(':'))
+    return sum([hours * 60 ** 2, minutes * 60, secs]) * 1000
 
 
 @app.route('/imgs/<path:filename>')
