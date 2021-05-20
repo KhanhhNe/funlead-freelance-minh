599baf917159f2908f20b78cefb3155c84dde59f
KhanhhNe <khanhluong3005@gmail.com>
2021-05-20 17:01:15 +0700

    Dynamic calculation of bit range.

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import os
-from datetime import time
 
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_route():
-    if request.method == 'GET':
-        imgs = [os.path.join('imgs', filename) for filename in os.listdir('imgs') if 'labelled' not in filename]
-        time_offset = sum([14 * 60**2, 16 * 60, 4]) * 1000
-
-        return render_template('index.html', imgs=imgs[:1], time_offset=time_offset, pixel_scale=10)
+    imgs = [os.path.join('imgs', filename) for filename in os.listdir('imgs') if 'labelled' not in filename]
+    if request.args.get('time_offset'):
+        time_offset = request.args['time_offset']
     else:
-        pass
+        time_offset = '14:16:04'
+    hours, minutes, secs = map(int, time_offset.split(':'))
+    time_offset = sum([hours * 60**2, minutes * 60, secs]) * 1000
+
+    bit_start = request.args.get('bit_start', 0)
+    bit_end = request.args.get('bit_end', 15)
+
+    return render_template(
+        'index.html', imgs=imgs[:1], pixel_scale=10,
+        time_offset=time_offset,
+        bit_start=bit_start,
+        bit_end=bit_end
+    )
 
 
 @app.route('/imgs/<path:filename>')
